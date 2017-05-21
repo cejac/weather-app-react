269b03ee78134b8bc4508260fbbf9b0590ef5db5
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-05-21 15:02:10 -0700

    redux promise practice

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const API_KEY = '83d353252b186ac27b4cb2cbc542e917'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
-const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`
